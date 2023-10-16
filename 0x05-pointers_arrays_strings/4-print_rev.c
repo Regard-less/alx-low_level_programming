30421ae9326df2d05fe49e20f15a05bba5ba7ffa
Your Name <you@example.com>
2023-10-16 16:12:50 -0700

    new

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * print_rev - imprime en reversa
  * @s: string
@@ -21,5 +20,7 @@ void print_rev(char *s)
 		_putchar(*s);
 		s--;
 	}
+
 	_putchar('\n');
 }
+
